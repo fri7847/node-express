a7875e17b09c2cc1672b6b721523b23d260b4693
fri7847 <80796724+fri7847@users.noreply.github.com>
2023-01-19 11:08:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var index = require('./view/index');
+var index = require('./views/index');
 
 var app = express();
 
