10e1ab4d743ba421f53997675ccac279d7124958
fri7847 <80796724+fri7847@users.noreply.github.com>
2023-01-19 11:02:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', function(req,res){
+  req.send("test")
+});
+app.use('/users', function(req,res){
+  req.send("tes2t")
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
